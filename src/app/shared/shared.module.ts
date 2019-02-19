074fe85e5a5dce9f5a620a4ebb0208cd38c3552e
Suresh Selvaraj <suresh.selvaraj5@cognizant.com>
2019-02-19 19:44:04 +0530

    routerLink added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { CommonMaterialModule } from './common-material/common-material.module';
 
+
 @NgModule({
   declarations: [ HeaderComponent, FooterComponent, SideNavComponent ],
   imports: [
     CommonModule,
-    CommonMaterialModule
+    CommonMaterialModule,
+    RouterModule
   ],
   exports: [HeaderComponent, FooterComponent, SideNavComponent]
 })
