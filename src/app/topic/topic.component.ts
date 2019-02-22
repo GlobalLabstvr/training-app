e547780fb64e4caa78360bde3013d2ce4b742159
Suresh Selvaraj <suresh.selvaraj5@cognizant.com>
2019-02-22 18:59:25 +0530

    mentoring added

--- src/app/topic/topic.component.ts
+++ src/app/topic/topic.component.ts
@@ -1,33 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { TopicService } from './topic.service';
 import { Topic } from './topic';
 import { Router, NavigationStart, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-topic',
   templateUrl: './topic.component.html',
   styleUrls: ['./topic.component.css']
 })
-export class TopicComponent implements OnInit{
+export class TopicComponent implements OnInit, OnDestroy {
   @Input()
   videoUrl = 'https://www.youtube.com/embed/4TC5s_xNKSs?list=PLH-xYrxjfO2VsvyQXfBvsQsufAzvlqdg9';
 
   topic: Topic;
-
+  show: boolean = false;
+  subscribtion: Subscription;
 
   constructor(private topicService: TopicService,
     private router: Router,
     private route: ActivatedRoute) {
-      this.router.events.subscribe((event: NavigationStart) => {
+      this.subscribtion = this.router.events.subscribe((event: NavigationStart) => {
         
         if(route.snapshot.params['id'] !== undefined){
-          console.log('in topic cons'+ route.snapshot.params['id']);
+          console.log('in topic cons'+ route.snapshot.params['id']+":"+this.router.url);
           this.getTopics(route.snapshot.params['id']);
         }
         else{
           console.log('navigation started:'+this.router.url)
         }
-        
       });
   }
 
@@ -54,5 +55,10 @@ export class TopicComponent implements OnInit{
 
   openUploadDialog(){
     
+  }
+
+  ngOnDestroy(){
+    console.log("unscribe");
+    this.subscribtion.unsubscribe();
   }
 }
--- src/app/topic/topic.ts
+++ src/app/topic/topic.ts
@@ -1,8 +1,10 @@
 import { Playlist } from './Playlist';
+import { Chapter } from './Chapter';
 
 export interface Topic {
     id: string;
     name: string;
     description: string;
     playlist: [Playlist];
+    chapter: [Chapter];
 }
