2b53e66b090f6b84c8bd3909a543061a013ff8d2
Suresh Selvaraj <suresh.selvaraj5@cognizant.com>
2019-02-26 17:21:52 +0530

    change in the UrlConstants

--- src/app/shared/model/url-constants.ts
+++ src/app/shared/model/url-constants.ts
@@ -1,5 +1,8 @@
 export class UrlConstants {
 
-   public static get coursesUrl(): string { return "https://json-server-tvr-digital.appspot.com/courses/"; }
-   public static get topicsUrl(): string { return "https://json-server-tvr-digital.appspot.com/topics/"; }
+   //public static get coursesUrl(): string { return "https://json-server-tvr-digital.appspot.com/courses/"; }
+   //public static get topicsUrl(): string { return "https://json-server-tvr-digital.appspot.com/topics/"; }
+
+   public static get coursesUrl(): string { return "http://localhost:3000/courses/"; }
+   public static get topicsUrl(): string { return "http://localhost:3000/topics/"; }
 }
