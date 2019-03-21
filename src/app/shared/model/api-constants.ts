28236121438f272d49b3261809442ccf417d7e00
Kathambaritvr <sujeesatheeshveera@gmail.com>
2019-03-21 17:01:48 +0530

    integrated with api

--- src/app/shared/model/api-constants.ts
+++ src/app/shared/model/api-constants.ts
@@ -1,8 +1,13 @@
+
+
 export class APIConstants {
 
    //public static get coursesUrl(): string { return "https://json-server-tvr-digital.appspot.com/courses/"; }
    //public static get topicsUrl(): string { return "https://json-server-tvr-digital.appspot.com/topics/"; }
 
-   public static get coursesUrl(): string { return "http://localhost:3000/courses/"; }
-   public static get topicsUrl(): string { return "http://localhost:3000/topics/"; }
+   public static get coursesUrl(): string { return "http://localhost:8080/courses/"; }
+   public static get topicsUrl(): string { return "http://localhost:8080/topics/"; }
+   
+  
+ 
 }
